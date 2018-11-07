2f98e4c4818d30cc29034d5e079abe0ce5bf6bb5
Emil Petersson <emilpet93@gmail.com>
2018-11-07 14:05:45 +0100

    Some changes, including a printed message for the user

--- javascript.js
+++ javascript.js
@@ -1,21 +1,37 @@
-const roll = Math.floor(Math.random() * 6);
-console.log(roll);
+// Roll a random number between 1 and 6
+const roll = Math.ceil(Math.random() * 6);
 
 document.addEventListener('DOMContentLoaded', function() {
-    var diceContainer = document.getElementById('dice');
-    var displayDice = document.createElement('div');
+    // Make the dice a child of the container
+    let diceContainer = document.getElementById('dice');
+    let displayDice = document.createElement('div');
     diceContainer.appendChild(displayDice);
-    if (roll == 0) {
-        displayDice.id = "diceOne";
-    } else if (roll == 1) {
-        displayDice.id = "diceTwo";
-    } else if (roll == 2) {
-        displayDice.id = "diceThree";
-    } else if (roll == 3) {
-        displayDice.id = "diceFour";
-    } else if (roll == 4) {
-        displayDice.id = "diceFive";
-    } else if (roll == 5) {
-        displayDice.id = "diceSix";
+
+    // Create a message and print it out
+    let msgHolder = document.getElementsByTagName('aside');
+    let displayMsg = document.createElement('p');
+    msgHolder[0].appendChild(displayMsg);
+    let message = `You rolled ${roll} <br> Please reload the page to roll again.`;
+    displayMsg.innerHTML = message;
+
+    // Display dice with the rolled number
+    switch(roll) {
+        case 1:
+          displayDice.id = "diceOne";
+          break;
+        case 2:
+          displayDice.id = "diceTwo";
+          break;
+        case 3:
+          displayDice.id = "diceThree";
+          break;
+        case 4:
+          displayDice.id = "diceFour";
+          break;
+        case 5:
+          displayDice.id = "diceFive";
+          break;
+        default:
+          displayDice.id = "diceSix";
     }
 });
