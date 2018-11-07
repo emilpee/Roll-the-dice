1ff7240db1aa7de8c28c11feb380fbd384b6c2bc
Emil Petersson <emilpet93@gmail.com>
2018-11-07 12:50:36 +0100

    Fixed positions of the dice sides

--- javascript.js
+++ javascript.js
@@ -3,9 +3,19 @@ console.log(roll);
 
 document.addEventListener('DOMContentLoaded', function() {
     var diceContainer = document.getElementById('dice');
-    var displayDice = document.createElement('img');
+    var displayDice = document.createElement('div');
     diceContainer.appendChild(displayDice);
-    if (roll > 0) {
+    if (roll == 0) {
         displayDice.id = "diceOne";
+    } else if (roll == 1) {
+        displayDice.id = "diceTwo";
+    } else if (roll == 2) {
+        displayDice.id = "diceThree";
+    } else if (roll == 3) {
+        displayDice.id = "diceFour";
+    } else if (roll == 4) {
+        displayDice.id = "diceFive";
+    } else if (roll == 5) {
+        displayDice.id = "diceSix";
     }
 });
